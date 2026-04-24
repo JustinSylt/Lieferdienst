9e231d6a8a04bc6b3b9f3e6facddc49447feb313
JustinSylt <justin.sylt@outlook.de>
2026-04-24 21:53:15 +0200

    Edit: Replaced file names and added css files

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-// init function that calls render functions
 function init() {
-  renderRestaurantDescription();
   renderChickenSliders();
 }
 
 // constants
+
 const CHICKEN_SLIDER_ARR = restaurantJFC.menu['Chicken Sliders'];
 const BUCKETS_AND_BOXES_ARR = restaurantJFC.menu['Buckets & Boxes'];
 const SIDES_AND_DIPS_ARR = restaurantJFC.menu['Sides & Dips'];
 
-// render Chicken Sliders section
 function renderChickenSliders() {
-  let chickenSliderRef = document.getElementById('chicken-slider-container');
+  let chickenSliderRef = document.getElementById('chicken-slider');
+
   for (let i = 0; i < CHICKEN_SLIDER_ARR.length; i++) {
-    let itemCard = renderItemCardTemplatete(
+    let chickenSliderTemplate = renderMenuItemsTemplate(
       CHICKEN_SLIDER_ARR[i].title,
       CHICKEN_SLIDER_ARR[i].description,
       CHICKEN_SLIDER_ARR[i].price,
     );
-    chickenSliderRef.innerHTML += itemCard;
+    chickenSliderRef.innerHTML += chickenSliderTemplate;
   }
 }
-
-// render Buckets & Boxes section
-function renderBucketsAndBoxes() {}
-
-// render Sides & Dips section
-function renderSidesAndDips() {}
--- scripts/template.js
+++ scripts/template.js
@@ -1,21 +1,14 @@
-// render restaurant description
-function renderRestaurantDescription() {
-  let restaurantDescriptionRef = document.getElementById(
-    'restaurant-description',
-  );
-  restaurantDescriptionRef.innerHTML = `
-          <h1>${restaurantJFC.restaurantDetails.name}</h1>
-          <p>${restaurantJFC.restaurantDetails.description}</p>
-          <p>${restaurantJFC.restaurantDetails.rating} / 5.0 ☆</p>`;
-}
-
-function renderItemCardTemplatete(title, description, price) {
+function renderMenuItemsTemplate(title, description, price) {
   return `<div class="item-card">
-            <div class="item-description">
-              <h3>${title}</h3>
-              <p>${description}</p>
-              <p>$${price}</p>
-            </div>
-            <button class="add-item-button">Add to Basket</button>
-          <div>`;
+              <div class="item-description">
+                <h3>${title}</h3>
+                <p>
+                  ${description}
+                </p>
+              </div>
+              <div class="btn-price">
+                <p>$${price}</p>
+                <button class="add-item-btn">Add to basket</button>
+              </div>
+          </div>`;
 }
