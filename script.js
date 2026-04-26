61b2805cfeaa2a131d6cbb113fa2ebdc06688bab
JustinSylt <justin.sylt@outlook.de>
2026-04-26 21:27:36 +0200

    Add: filter item from itemID out of db.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function init() {
-	renderChickenSliders();
-	renderBucketsAndBoxes();
-	renderSidesAndDips();
-	renderBasketContent();
+  renderChickenSliders();
+  renderBucketsAndBoxes();
+  renderSidesAndDips();
+  renderBasketContent();
 }
 
 // constants
@@ -12,69 +12,80 @@ const SIDES_AND_DIPS_ARR = restaurantJFC.menu['Sides & Dips'];
 
 // render all Chicken Sliders from db.js
 function renderChickenSliders() {
-	let chickenSliderRef = document.getElementById('chicken-slider');
+  let chickenSliderRef = document.getElementById('chicken-slider');
 
-	for (let i = 0; i < CHICKEN_SLIDER_ARR.length; i++) {
-		let chickenSliderTemplate = renderMenuItemsTemplate(
-			CHICKEN_SLIDER_ARR[i].title,
-			CHICKEN_SLIDER_ARR[i].description,
-			CHICKEN_SLIDER_ARR[i].price,
-		);
-		chickenSliderRef.innerHTML += chickenSliderTemplate;
-	}
+  for (let i = 0; i < CHICKEN_SLIDER_ARR.length; i++) {
+    let chickenSliderTemplate = renderMenuItemsTemplate(
+      CHICKEN_SLIDER_ARR[i].title,
+      CHICKEN_SLIDER_ARR[i].description,
+      CHICKEN_SLIDER_ARR[i].price,
+      CHICKEN_SLIDER_ARR[i].itemID,
+    );
+    chickenSliderRef.innerHTML += chickenSliderTemplate;
+  }
 }
 
 // render all Buckets and Boxes from db.js
 function renderBucketsAndBoxes() {
-	let bucketsAndBoxesRef = document.getElementById('buckets-and-boxes');
+  let bucketsAndBoxesRef = document.getElementById('buckets-and-boxes');
 
-	for (let i = 0; i < BUCKETS_AND_BOXES_ARR.length; i++) {
-		let bucketsAndBoxesTemplate = renderMenuItemsTemplate(
-			BUCKETS_AND_BOXES_ARR[i].title,
-			BUCKETS_AND_BOXES_ARR[i].description,
-			BUCKETS_AND_BOXES_ARR[i].price,
-		);
-		bucketsAndBoxesRef.innerHTML += bucketsAndBoxesTemplate;
-	}
+  for (let i = 0; i < BUCKETS_AND_BOXES_ARR.length; i++) {
+    let bucketsAndBoxesTemplate = renderMenuItemsTemplate(
+      BUCKETS_AND_BOXES_ARR[i].title,
+      BUCKETS_AND_BOXES_ARR[i].description,
+      BUCKETS_AND_BOXES_ARR[i].price,
+      BUCKETS_AND_BOXES_ARR[i].itemID,
+    );
+    bucketsAndBoxesRef.innerHTML += bucketsAndBoxesTemplate;
+  }
 }
 
 // render all Sides and Dips from db.js
 function renderSidesAndDips() {
-	let sidesAndDipsRef = document.getElementById('sides-and-dips');
+  let sidesAndDipsRef = document.getElementById('sides-and-dips');
 
-	for (let i = 0; i < SIDES_AND_DIPS_ARR.length; i++) {
-		let sidesAndDipsTemplate = renderMenuItemsTemplate(
-			SIDES_AND_DIPS_ARR[i].title,
-			SIDES_AND_DIPS_ARR[i].description,
-			SIDES_AND_DIPS_ARR[i].price,
-		);
+  for (let i = 0; i < SIDES_AND_DIPS_ARR.length; i++) {
+    let sidesAndDipsTemplate = renderMenuItemsTemplate(
+      SIDES_AND_DIPS_ARR[i].title,
+      SIDES_AND_DIPS_ARR[i].description,
+      SIDES_AND_DIPS_ARR[i].price,
+      SIDES_AND_DIPS_ARR[i].itemID,
+    );
 
-		sidesAndDipsRef.innerHTML += sidesAndDipsTemplate;
-	}
+    sidesAndDipsRef.innerHTML += sidesAndDipsTemplate;
+  }
 }
 
 // render basket content
 function renderBasketContent() {
-	let itemsInBaskets = calculateBasketItems();
-	let basketItemsRef = document.getElementById('basket-items');
+  let itemsInBaskets = calculateBasketItems();
+  let basketItemsRef = document.getElementById('basket-items');
 
-	if (itemsInBaskets == 0) {
-		basketItemsRef.innerHTML = '';
-		basketItemsRef.innerHTML += renderEmptyBasketTemplate();
-	} else {
-	}
+  if (itemsInBaskets == 0) {
+    basketItemsRef.innerHTML = '';
+    basketItemsRef.innerHTML += renderEmptyBasketTemplate();
+  } else {
+  }
 }
 
 // calculate basket count
 function calculateBasketItems() {
-	let basketCount = 0;
-	const categories = Object.keys(restaurantJFC.menu);
+  let basketCount = 0;
+  const categories = Object.keys(restaurantJFC.menu);
 
-	for (let i = 0; i < categories.length; i++) {
-		const items = restaurantJFC.menu[categories[i]];
-		for (let j = 0; j < items.length; j++) {
-			basketCount += items[j].count;
-		}
-	}
-	return basketCount;
+  for (let i = 0; i < categories.length; i++) {
+    const items = restaurantJFC.menu[categories[i]];
+    for (let j = 0; j < items.length; j++) {
+      basketCount += items[j].count;
+    }
+  }
+  return basketCount;
+}
+
+// Increment counter in object
+
+function getItemFromDb(itemID) {
+  let allItems = Object.values(restaurantJFC.menu).flat();
+  let item = allItems.find((item) => item.itemID === itemID);
+  console.log(item);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,5 @@
-function renderMenuItemsTemplate(title, description, price) {
-	return `<div class="item-card">
+function renderMenuItemsTemplate(title, description, price, itemID) {
+  return `<div class="item-card">
               <div class="item-description">
                 <h3>${title}</h3>
                 <p>
@@ -8,12 +8,12 @@ function renderMenuItemsTemplate(title, description, price) {
               </div>
               <div class="btn-price">
                 <p>$${price}</p>
-                <button class="add-item-btn">Add to basket</button>
+                <button class="add-item-btn" onClick="getItemFromDb(${itemID})">Add to basket</button>
               </div>
           </div>`;
 }
 
 function renderEmptyBasketTemplate() {
-	return `<p>Seems empty in here...</p>
+  return `<p>Seems empty in here...</p>
           <p>Add something to your Basket!</p>`;
 }
